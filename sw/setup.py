e99c1a4739ff38e59a1872d6794a0cb839a74288
mehrabmahdian <memahdian@outlook.com>
2025-08-18 14:48:53 +0300

    Initial commit

---
+++ sw/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="InverseDesign",
+    version="1.0",
+    author="Mehrab Mahdian",
+    author_email="mehrab.mahdian@taltech.ee",
+    description="InverseDesign",
+    long_description="A python module for inverse design in electrospinning",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "pandas",
+        "xgboost",
+        "scikit-learn",
+        "seaborn"
+    ],
+    python_requires='>=3.8',
+)
